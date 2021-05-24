b968cd7d07396c91040f91cbbdfb82aae23ca8e1
loesberg <jesseloesberg@gmail.com>
2021-05-24 14:45:16 -0700

    various

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@
      'use strict';
 
 
-     // var app = angular.module('viewCustom', ['angularLoad']);
+     var app = angular.module('viewCustom', ['angularLoad']);
 
      /****************************************************************************************************/
 
