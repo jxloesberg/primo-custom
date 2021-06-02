e057e327c4db0c63eb707f52c3540fad03bf25c6
loesberg <jesseloesberg@gmail.com>
2021-06-02 12:33:03 -0700

    temporarily remove wordmark

--- js/custom.js
+++ js/custom.js
@@ -18,29 +18,29 @@
  * https://github.com/dswalker/csu-central-package/
  *
  */
-     app.component('prmSearchBarAfter', {
-          bindings: { parentCtrl: '<' },
-          controller: 'SearchBarAfterController',
-          templateUrl: 'custom/01UCS_BER-JESSE_TEST/html/prmSearchBarAfter.html',
-     });
-
-     app.controller('SearchBarAfterController', ['$location', '$window', function($location, $window){
-          this.navigateToHomePage = function () {
-               var params = $location.search();
-               var vid = params.vid;
-               var lang = params.lang || "en_US";
-               var split = $location.absUrl().split('/discovery/');
-
-               if (split.length === 1) {
-                    console.log(split[0] + ' : Could not detect the view name!');
-                    return false;
-               }
-
-               var baseUrl = split[0];
-               $window.location.href = baseUrl + '/discovery/search?vid=' + vid + '&lang=' + lang;
-               return true;
-          };
-     }]);
+     // app.component('prmSearchBarAfter', {
+     //      bindings: { parentCtrl: '<' },
+     //      controller: 'SearchBarAfterController',
+     //      templateUrl: 'custom/01UCS_BER-JESSE_TEST/html/prmSearchBarAfter.html',
+     // });
+     //
+     // app.controller('SearchBarAfterController', ['$location', '$window', function($location, $window){
+     //      this.navigateToHomePage = function () {
+     //           var params = $location.search();
+     //           var vid = params.vid;
+     //           var lang = params.lang || "en_US";
+     //           var split = $location.absUrl().split('/discovery/');
+     //
+     //           if (split.length === 1) {
+     //                console.log(split[0] + ' : Could not detect the view name!');
+     //                return false;
+     //           }
+     //
+     //           var baseUrl = split[0];
+     //           $window.location.href = baseUrl + '/discovery/search?vid=' + vid + '&lang=' + lang;
+     //           return true;
+     //      };
+     // }]);
 
 
 })();
